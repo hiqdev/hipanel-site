031872acf2b52e863e5b36837dd53daa301be53e
Andrii Vasyliev <sol@hiqdev.com>
2016-08-21 17:10:31 +0000

    inited empty tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Selling site for HiPanel
+ *
+ * @link      http://hipanel.com/
+ * @package   hipanel-site
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+$bootstrap = __DIR__ . '/../src/_bootstrap.php';
+
+require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
