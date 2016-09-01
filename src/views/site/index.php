828757dbb4473c2d80fec14b95b3312a2f0afa06
tofid <andreyklochok@gmail.com>
2016-09-01 18:45:25 +0300

    Changed main menu

---
+++ src/views/site/index.php
@@ -0,0 +1,30 @@
+<?php
+use hipanel\site\widgets\DomainPriceTable;
+use hipanel\site\widgets\DomainSearchForm;
+use hisite\modules\news\widgets\NewsRotatorWidget;
+use yii\helpers\Html;
+
+?>
+
+<div class="domainavailability">
+    <div class="row">
+        <div class="col-sm-12 col-md-9 center-block">
+            <?= Html::tag('h1', $this->title, ['class' => 'text-center']) ?>
+            <div class="domain-form-container">
+                <?= DomainSearchForm::widget() ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="domain-prices">
+    <div class="row">
+        <div class="row">
+            <div class="col-sm-12">
+                <?= DomainPriceTable::widget() ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= NewsRotatorWidget::widget() ?>
