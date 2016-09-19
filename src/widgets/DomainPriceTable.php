95b85198fccf8c6d0952c1bb46aaf55c8efc4145
Andrii Vasyliev <sol@hiqdev.com>
2016-09-19 13:09:04 +0000

    fixed php warning

--- src/widgets/DomainPriceTable.php
+++ src/widgets/DomainPriceTable.php
@@ -4,8 +4,8 @@ namespace hipanel\site\widgets;
 
 use hipanel\modules\domain\models\Domain;
 use hipanel\modules\finance\models\Tariff;
-use yii\base\Widget;
 use hipanel\site\helpers\SiteHelper;
+use yii\base\Widget;
 
 class DomainPriceTable extends Widget
 {
@@ -18,14 +18,16 @@ class DomainPriceTable extends Widget
         $domainZones = Domain::perform('GetZones', [], true);
         $domains = SiteHelper::domain($domains['resources'], $domainZones);
         $promotion = Tariff::perform('GetInfo', ['id' => 7312138]);
-//        if (!err::is($promoution)) tpl::set('promoution', resources::domain($promoution['resources'], tpl::get('zones')));
 
         foreach (['domains', 'promotion'] as $price) {
             $zones = $$price;
-            foreach ($zones as &$zone) {
-                if (is_array($zone)) {
-                    foreach ($zone as $operation => $info) {
-                        if (!in_array($operation, ['dregistration', 'drenewal', 'dtransfer'])) unset($zone[$operation]);
+
+            if (is_array($zones)) {
+                foreach ($zones as &$zone) {
+                    if (is_array($zone)) {
+                        foreach ($zone as $operation => $info) {
+                            if (!in_array($operation, ['dregistration', 'drenewal', 'dtransfer'])) unset($zone[$operation]);
+                        }
                     }
                 }
             }
