08301936ea02bb5de05187223d2cd214159ba3ad
Andrii Vasyliev <sol@hiqdev.com>
2016-09-20 15:18:22 +0000

    simplified SiteController, extended from hipanel site controller

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -5,23 +5,15 @@ namespace hipanel\site\controllers;
 use hipanel\modules\domain\cart\DomainRegistrationProduct;
 use hipanel\modules\server\helpers\ServerHelper;
 use hipanel\modules\server\cart\Tariff;
+use hipanel\modules\server\cart\ServerOrderProduct;
 use hiqdev\yii2\cart\actions\AddToCartAction;
 use hisite\actions\RenderAction;
-use hipanel\models\User;
-use hipanel\modules\server\cart\ServerOrderProduct;
-use Yii;
-use yii\authclient\AuthAction;
-use yii\authclient\ClientInterface;
 
-class SiteController extends \hisite\controllers\SiteController
+class SiteController extends \hipanel\controllers\SiteController
 {
     public function actions()
     {
         return array_merge(parent::actions(), [
-            'auth' => [
-                'class' => AuthAction::class,
-                'successCallback' => [$this, 'onAuthSuccess'],
-            ],
             'index' => [
                 'class' => RenderAction::class,
             ],
@@ -51,34 +43,4 @@ class SiteController extends \hisite\controllers\SiteController
         ]);
     }
 
-    public function onAuthSuccess(ClientInterface $client)
-    {
-        $attributes = $client->getUserAttributes();
-        $user = new User();
-        foreach ($user->attributes() as $k) {
-            if (isset($attributes[$k])) {
-                $user->{$k} = $attributes[$k];
-            }
-        }
-        $user->save();
-        Yii::$app->user->login($user, 3600 * 24 * 30);
-    }
-
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goBack();
-        }
-
-        return $this->redirect(['/site/auth', 'authclient' => 'hiam']);
-    }
-
-    public function actionLogout()
-    {
-        $back = Yii::$app->request->getHostInfo();
-        $url = Yii::$app->authClientCollection->getClient()->buildUrl('site/logout', compact('back'));
-        Yii::$app->user->logout();
-
-        return Yii::$app->response->redirect($url);
-    }
 }
