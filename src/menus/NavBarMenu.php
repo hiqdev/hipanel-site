a76bce6df7d0b060311ce71cb606368c1ebcb7c9
tofid <andreyklochok@gmail.com>
2016-09-21 15:10:34 +0300

    Added getGravater private method which is embedded to items method

--- src/menus/NavBarMenu.php
+++ src/menus/NavBarMenu.php
@@ -2,6 +2,7 @@
 
 namespace hipanel\site\menus;
 
+use hipanel\widgets\Gravatar;
 use hiqdev\yii2\cart\widgets\PanelTopCart;
 use hiqdev\yii2\language\widgets\LanguageMenu;
 use Yii;
@@ -28,8 +29,30 @@ class NavBarMenu extends \hiqdev\menumanager\Menu
                 ]
             ],
             ['label' => Yii::t('hisite', 'Login'), 'url' => ['/site/login'], 'visible' => Yii::$app->user->isGuest],
-            ['label' => Yii::t('hisite', 'Panel'), 'url' => ['#'], 'visible' => !Yii::$app->user->isGuest],
-            ['label' => Yii::t('hisite', 'Logout'), 'url' => ['/site/logout'], 'visible' => !Yii::$app->user->isGuest],
+            ['label' => Yii::t('hisite', 'Panel'), 'url' => '#', 'visible' => !Yii::$app->user->isGuest],
+            [
+                'label' => $this->getGravatar() . '&nbsp;&nbsp;' . Yii::$app->user->identity->username . ' (' . Yii::t('hisite', 'Logout') . ')',
+                'url' => ['/site/logout'],
+                'visible' => !Yii::$app->user->isGuest,
+                'encode' => false,
+            ],
         ];
+    }
+
+    private function getGravatar()
+    {
+        $out = '';
+        $email = @Yii::$app->user->identity->email;
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $out = Gravatar::widget([
+                'size' => 18,
+                'email' => $email,
+                'options' => [
+                    'alt' => Yii::$app->user->identity->username,
+                ],
+            ]);
+        }
+
+        return $out;
     }
 }
