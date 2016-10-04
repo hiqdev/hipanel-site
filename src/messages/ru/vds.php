17c8f7379ae049d58eb3f63b7f6605b8246a978d
tofid <andreyklochok@gmail.com>
2016-10-04 14:58:26 +0300

    Added new translation and new dictionary - vds

--- src/messages/ru/vds.php
+++ src/messages/ru/vds.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tofid
- * Date: 04.10.16
- * Time: 14:10
- */
+
+return [
+    'VDS Pricing' => 'Цены на VDS',
+    'The optimal solution for small projects, personal blogs, online business cards.' => 'Оптимальное решение для небольших проектов, личных блогов, сайтов-визиток.',
+    'The most popular and universal rate which is suitable for most online stores and websites.' => 'Самый популярный и универсальный тариф, который подходит для большинства интернет-магазинов и веб-сайтов.',
+    'Intended for complex projects that require significant resources and administration.' => 'Предназначен для сложных проектов, требующих значительных ресурсов и администрирования.',
+    'This VDS is advanced solution for demanding websites and scripts that require higher performance.' => 'Это VDS - передовое решение для требовательных веб-сайтов и скриптов, которые требуют более высокую производительность.',
+    'It\'s a great alternative to dedicated server by performance and reliability. Includes administration and 24 * 7 monitoring.' => 'Это отличная альтернатива выделенному серверу по производительности и надежности. Включает в себя администрирование и 24/7 мониторинг.',
+    'VDS for small traffic projects' => 'VDS для небольших проектов',
+    'The configuration of this virtual server suits the websites and scripts that generate considerable traffic and require the efficiency of the server' => 'Конфигурация этого виртуального сервера устраивает веб-сайты и скрипты, которые генерируют значительный трафик и требуют эффективности работы сервера',
+    'This VDS is intended for websites with big traffic or websites on CMS Bitrix, Drupal' => 'Этот VDS предназначен для сайтов с большим трафиком или веб-сайтов на CMS Drupal, Bitrix',
+    'The main advantage of a VDS based on XEN with SSD is speed. It is more than 250 times faster than a conventional HDD. Due to Xen virtualization type, all resources are assigned to user and the operation of your VDS does not depend on the main server\'s load.' => 'Основное преимущество VDS на базе Xen с применением SSD — скорость работы, которая выше более чем в 250 раз по сравнению с обычными HDD. Благодаря типу виртуализации Xen, все ресурсы гарантированно закреплены за пользователем, и работа вашего VDS не зависит от общей нагрузки на сервер.',
+    'Virtual dedicated server based on Xen is a perfect solution for most medium and large projects because of its performance that is highly competitive with the performance of a dedicated server.' => 'Виртуальный выделенный сервер на базе Xen — идеальное решение для большинства проектов средней и большой сложности, так как порой не уступает по производительности выделенному серверу.',
+    'VPS based on OpenVZ — is an inexpensive and reliable solution for small projects that do not require many resources (HTML web-sites, landing pages, small blogs, personal websites, business cards, etc.). An additional advantage of our VPS based on OpenVZ is utilization of SSD cache system that improves performance of the disk subsystem during frequently accessed data readings.' => 'VDS на базе OpenVZ — это недорогой и надежный вариант для размещения проектов, не требующих большого количества ресурсов (HTML-сайты, лендинги, небольшие блоги, сайты-визитки и пр.). Дополнительным преимуществом наших VDS на базе OpenVZ является использование системы кеширования с SSD, что увеличивает производительность дисковой подсистемы на чтение часто запрашиваемых данных.',
+    'Hosting by {name} is an optimal and reliable solution for any project at affordable price.' => 'Хостинг от {name} — это оптимальное и надежное решение для любых проектов по доступным ценам',
+];
