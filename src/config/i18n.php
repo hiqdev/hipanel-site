403e712b9bf17351fb4cc5813f8ff9606174c4ce
tofid <andreyklochok@gmail.com>
2016-10-04 18:46:37 +0300

    Added i18n config

---
+++ src/config/i18n.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'sourcePath' => '@hipanel/site',
+    'languages' => ['ru'],
+    'removeUnused' => true,
+    'markUnused' => false,
+    'sort' => true,
+    'messagePath' => '@hipanel/site/messages',
+    'ignoreCategories' => [
+        'app',
+        'cart',
+        'hisite',
+        'hipanel/server/order',
+        'hipanel/server/os',
+    ]
+];
