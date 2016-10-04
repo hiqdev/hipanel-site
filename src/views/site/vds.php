a3b7de848978136a37cf560f28c5a5339e1454dc
tofid <andreyklochok@gmail.com>
2016-10-04 13:37:28 +0300

    Fixed popover on vds tables

--- src/views/site/vds.php
+++ src/views/site/vds.php
@@ -6,6 +6,7 @@
  */
 $this->title = Yii::t('hisite', 'VDS Pricing');
 $this->registerCss(".products-table { display: table; }");
+$this->registerJs("$('.shared-table [data-toggle=\"popover\"]').popover();");
 ?>
 
 <?= $this->render('_hostingPriceBox', [
