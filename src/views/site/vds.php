ad8645524108460dd50512aedbb4a11094fab40f
tofid <andreyklochok@gmail.com>
2016-10-05 12:17:22 +0300

    Fixed dictionaries in vds

--- src/views/site/vds.php
+++ src/views/site/vds.php
@@ -8,8 +8,8 @@ use hipanel\site\HipanelSiteAsset;
 
 HipanelSiteAsset::register($this);
 
-$this->title = Yii::t('hipanel/vds', 'VDS Pricing');
-$this->blocks['subTitle'] = Yii::t('hipanel/vds', 'Hosting by {name} is an optimal and reliable solution for any project at affordable price.', ['name' => Yii::$app->params['organizationName']]);
+$this->title = Yii::t('hipanel/site/vds', 'VDS Pricing');
+$this->blocks['subTitle'] = Yii::t('hipanel/site/vds', 'Hosting by {name} is an optimal and reliable solution for any project at affordable price.', ['name' => Yii::$app->params['organizationName']]);
 
 $this->registerCss(".products-table { display: table; }");
 $this->registerJs("$('.shared-table [data-toggle=\"popover\"]').popover();");
@@ -23,10 +23,10 @@ $this->registerJs("$('.shared-table [data-toggle=\"popover\"]').popover();");
         </div>
         <div class="col-md-9 wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="200ms">
             <p>
-                <?= Yii::t('hipanel/vds', 'The main advantage of a VDS based on XEN with SSD is speed. It is more than 250 times faster than a conventional HDD. Due to Xen virtualization type, all resources are assigned to user and the operation of your VDS does not depend on the main server\'s load.') ?>
+                <?= Yii::t('hipanel/site/vds', 'The main advantage of a VDS based on XEN with SSD is speed. It is more than 250 times faster than a conventional HDD. Due to Xen virtualization type, all resources are assigned to user and the operation of your VDS does not depend on the main server\'s load.') ?>
             </p>
             <p>
-                <?= Yii::t('hipanel/vds', 'Virtual dedicated server based on Xen is a perfect solution for most medium and large projects because of its performance that is highly competitive with the performance of a dedicated server.') ?>
+                <?= Yii::t('hipanel/site/vds', 'Virtual dedicated server based on Xen is a perfect solution for most medium and large projects because of its performance that is highly competitive with the performance of a dedicated server.') ?>
             </p>
         </div>
     </div>
@@ -41,7 +41,7 @@ $this->registerJs("$('.shared-table [data-toggle=\"popover\"]').popover();");
         </div>
         <div class="col-md-9 wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="200ms">
             <p>
-                <?= Yii::t('hipanel/vds', 'VPS based on OpenVZ — is an inexpensive and reliable solution for small projects that do not require many resources (HTML web-sites, landing pages, small blogs, personal websites, business cards, etc.). An additional advantage of our VPS based on OpenVZ is utilization of SSD cache system that improves performance of the disk subsystem during frequently accessed data readings.') ?>
+                <?= Yii::t('hipanel/site/vds', 'VPS based on OpenVZ — is an inexpensive and reliable solution for small projects that do not require many resources (HTML web-sites, landing pages, small blogs, personal websites, business cards, etc.). An additional advantage of our VPS based on OpenVZ is utilization of SSD cache system that improves performance of the disk subsystem during frequently accessed data readings.') ?>
             </p>
         </div>
     </div>
