59543b8ee6bc06d2cddc988cd471b37129f8a1fd
tofid <andreyklochok@gmail.com>
2016-10-18 12:51:31 +0300

    minor

--- src/views/faq/01-dns/02-setting/ru/01-a-record.php
+++ src/views/faq/01-dns/02-setting/ru/01-a-record.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tofid
- * Date: 17.10.16
- * Time: 15:09
- */
+$this->title = 'A-record';
+?>
+
+<p>You need to install our NS-servers to configure the A-record in the ahnames.com panel.</p>
+<p><a href="#01-ns#01-installing">Installing <?= $opt['host'] ?> NS-servers</a>.</p>
+<p>After selecting the A-record type, specify the correct IP address in the line that is shown on the
+    screenshot. </p>
+<p><img src="/www/img/help/dns/setup/en/a_1.png"></p>
+<p>You can also specify IP addresses for Subdomain names if necessary. To do that, the Subdomain name should be
+    indicated
+    as prefix in the specific field. If you need to specify only one IP address for all Subdomain names, write "*"
+    symbol in
+    the "Subdomain name" field.
+</p>
--- src/views/faq/01-dns/02-setting/ru/04-premium.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tofid
- * Date: 17.10.16
- * Time: 15:09
- */
