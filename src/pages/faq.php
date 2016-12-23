2d83c15f5da8b4dcf5fb3c5b824d83d7cba68f59
tofid <andreyklochok@gmail.com>
2016-12-23 18:38:20 +0200

    Fixed FaqMenu call

--- src/pages/faq.php
+++ src/pages/faq.php
@@ -1,9 +1,9 @@
 <?php
 
-use hiqdev\thememanager\widgets\Faq;
+use hipanel\site\menus\FaqMenu;
 
 $this->title = Yii::t('hipanel:site:faq', 'FAQ');
 $this->blocks['subTitle'] = Yii::t('hipanel:site:faq', 'Find answers to the common questions asked about our services')
 ?>
 
-<?= Faq::widget() ?>
+<?= FaqMenu::widget() ?>
