970b36abb9324fbc06148bf615e5ed676e716ed6
tofid <andreyklochok@gmail.com>
2017-02-06 19:38:57 +0200

    Rendered `domain` field by template

--- src/widgets/views/DomainSearchForm.php
+++ src/widgets/views/DomainSearchForm.php
@@ -15,19 +15,12 @@ $form = ActiveForm::begin([
     'fieldConfig' => [
         'template' => "{input}\n{hint}\n{error}",
     ],
-]) ?>
-<div class="row">
-    <input type="hidden" name="direct" value="true"/>
-    <div class="col-sm-11">
-        <?= $form->field($model, 'domain')->textInput([
-            'placeholder' => Yii::t('hipanel:domain', 'Domain search'),
-            'name' => 'domain',
-            'id' => 'domain',
-            'autocomplete' => 'off'
-        ]) ?>
-    </div>
-    <div class="col-sm-1">
-        <button class="mtr-btn button-fab" type="submit"><i class="fa fa-search"></i></button>
-    </div>
-</div>
-<?php ActiveForm::end() ?>
+]);
+print strtr($template, ['{field}' => $form->field($model, 'domain')->textInput([
+    'placeholder' => Yii::t('hipanel:domain', 'Domain search'),
+    'name' => 'domain',
+    'id' => 'domain',
+    'autocomplete' => 'off'
+])]);
+ActiveForm::end();
+?>
