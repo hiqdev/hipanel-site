fe472fe5a8852e7599cdd3fc9f6addd85e51b91a
tofid <andreyklochok@gmail.com>
2017-02-06 19:35:04 +0200

    Modified DomainPriceTable widget, remove getting data from widget

--- src/widgets/DomainPriceTable.php
+++ src/widgets/DomainPriceTable.php
@@ -9,32 +9,27 @@ use yii\base\Widget;
 
 class DomainPriceTable extends Widget
 {
+    public $domains = [];
+
+    public $promotion;
+
+    public $domainZones = [];
+
+    public $tableOptions = [];
+
+    private $defaultTableOptions = [
+        'id' => 'tld-table',
+        'class' => 'tablesaw',
+        'data-tablesaw-mode' => 'stack',
+    ];
+
     public function run()
     {
-        $domains = array_shift(Tariff::batchPerform('GetAvailableInfo', [
-            'seller' => SiteHelper::getSeller(),
-            'type' => 'domain',
-        ]));
-        $domainZones = Domain::batchPerform('GetZones', []);
-        $domains = SiteHelper::domain($domains['resources'], $domainZones);
-        $promotion = Tariff::perform('GetInfo', ['id' => 7312138]);
-
-        foreach (['domains', 'promotion'] as $price) {
-            $zones = $$price;
-
-            if (is_array($zones)) {
-                foreach ($zones as &$zone) {
-                    if (is_array($zone)) {
-                        foreach ($zone as $operation => $info) {
-                            if (!in_array($operation, ['dregistration', 'drenewal', 'dtransfer'])) unset($zone[$operation]);
-                        }
-                    }
-                }
-            }
-            $$price = $zones;
-        }
-
-        return $this->render('DomainPriceTable', compact('domains', 'promotion', 'domainZones'));
-
+        return $this->render('DomainPriceTable', [
+            'domains' => $this->domains,
+            'promotion' => $this->promotion,
+            'domainZones' => $this->domainZones,
+            'tableOptions' => array_merge($this->defaultTableOptions, $this->tableOptions),
+        ]);
     }
 }
