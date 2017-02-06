ee63b975893ba95ce0cf466b7c3bb48f9b8be244
tofid <andreyklochok@gmail.com>
2017-02-06 19:44:44 +0200

    Made Sailor index page

---
+++ src/themes/sailor/site/index.php
@@ -0,0 +1,56 @@
+<?php
+
+use hipanel\site\widgets\DomainPriceTable;
+use hipanel\site\widgets\DomainSearchForm;
+
+$this->title = Yii::t('hipanel:site:domain', 'Domain names search and registration');
+?>
+<section class="callaction">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="text-center">
+                    <h2><?= $this->title ?></h2>
+                </div>
+                <?= DomainSearchForm::widget() ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="text-center">
+                    <h2><?= Yii::t('hipanel:site:domain', 'Domain pricing') ?></h2>
+                    <p>
+                        <?= Yii::t('hipanel:site:domain', 'Today our company, offers the most competitive and fair prices for domain registration services. Due to the large number of domains that are in our care, we get a significant discount from our registrar. This creates a very positive impact on the price of domain names offered to you!') ?> </p>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="solidline">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?= DomainPriceTable::widget([
+                    'domains' => $domains,
+                    'promotion' => $promotion,
+                    'domainZones' => $domainZones,
+                    'tableOptions' => [
+                        'class' => 'table table-striped',
+                    ]
+                ]) ?>
+            </div>
+        </div>
+    </div>
+</section>
