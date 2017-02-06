5dec03b61df34d1da0a19cd0f6954f5a5da83a7d
tofid <andreyklochok@gmail.com>
2017-02-06 19:33:57 +0200

    Added template to render DomainSearchFrom widget

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -2,11 +2,10 @@
 
 use hipanel\site\widgets\DomainPriceTable;
 use hipanel\site\widgets\DomainSearchForm;
-use hisite\modules\news\widgets\NewsRotatorWidget;
 use yii\helpers\Html;
 
 $this->title = Yii::t('hipanel:site:domain', 'Domain names search and registration');
-
+$template = '<div class="row"><input type="hidden" name="direct" value="true"/><div class="col-sm-11">{field}</div><div class="col-sm-1"><button class="mtr-btn button-fab" type="submit"><i class="fa fa-search"></i></button></div></div>';
 ?>
 <?php $this->beginBlock('subHeader') ?>
     <div class="domainavailability">
@@ -14,7 +13,9 @@ $this->title = Yii::t('hipanel:site:domain', 'Domain names search and registrati
             <div class="col-sm-12 col-md-9 center-block">
                 <?= Html::tag('h1', $this->title, ['class' => 'text-center']) ?>
                 <div class="domain-form-container">
-                    <?= DomainSearchForm::widget() ?>
+                    <?= DomainSearchForm::widget([
+                        'template' => $template,
+                    ]) ?>
                 </div>
             </div>
         </div>
@@ -25,7 +26,7 @@ $this->title = Yii::t('hipanel:site:domain', 'Domain names search and registrati
     <div class="row">
         <div class="row">
             <div class="col-sm-12">
-                <?= DomainPriceTable::widget() ?>
+                <?= DomainPriceTable::widget(compact('domains', 'promotion', 'domainZones')) ?>
             </div>
         </div>
     </div>
