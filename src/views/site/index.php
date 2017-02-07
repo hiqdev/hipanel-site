0d7a38a057b37df9bd0a35ba843c0a16e675bbc0
tofid <andreyklochok@gmail.com>
2017-02-07 12:46:28 +0200

    minor

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -5,7 +5,6 @@ use hipanel\site\widgets\DomainSearchForm;
 use yii\helpers\Html;
 
 $this->title = Yii::t('hipanel:site:domain', 'Domain names search and registration');
-$template = '<div class="row"><input type="hidden" name="direct" value="true"/><div class="col-sm-11">{field}</div><div class="col-sm-1"><button class="mtr-btn button-fab" type="submit"><i class="fa fa-search"></i></button></div></div>';
 ?>
 <?php $this->beginBlock('subHeader') ?>
     <div class="domainavailability">
@@ -14,13 +13,28 @@ $template = '<div class="row"><input type="hidden" name="direct" value="true"/><
                 <?= Html::tag('h1', $this->title, ['class' => 'text-center']) ?>
                 <div class="domain-form-container">
                     <?= DomainSearchForm::widget([
-                        'template' => $template,
+                        'template' => '<div class="row"><input type="hidden" name="direct" value="true"/><div class="col-sm-11">{field}</div><div class="col-sm-1"><button class="mtr-btn button-fab" type="submit"><i class="fa fa-search"></i></button></div></div>',
                     ]) ?>
                 </div>
             </div>
         </div>
     </div>
 <?php $this->endBlock() ?>
+
+<div class="domaintlds">
+    <div class="row">
+        <div class="col-sm-12">
+            <h2 class="text-center"><?= Yii::t('hipanel:site:domain', 'Domain pricing') ?></h2>
+            <div class="titleborder centered">
+                <div class="titleborder_left"></div>
+                <div class="titleborder_sign"></div>
+                <div class="titleborder_right"></div>
+            </div>
+            <p class="text-center subtitle">
+                <?= Yii::t('hipanel:site:domain', 'Today our company, offers the most competitive and fair prices for domain registration services. Due to the large number of domains that are in our care, we get a significant discount from our registrar. This creates a very positive impact on the price of domain names offered to you!') ?> </p>
+        </div>
+    </div>
+</div>
 
 <div class="domain-prices">
     <div class="row">
@@ -31,3 +45,26 @@ $template = '<div class="row"><input type="hidden" name="direct" value="true"/><
         </div>
     </div>
 </div>
+
+<div class="domain-features-text">
+    <div class="row">
+        <div class="col-sm-6">
+            <h4><?= Yii::t('hipanel:site:domain', 'PAYMENT METHODS') ?></h4>
+        </div>
+        <div class="col-sm-6">
+            <p><?= Yii::t('hipanel:site:domain', 'We accept the following automatic payment methods') ?>:
+                <noindex>
+                    <a rel="nofollow" target="_blank" href="http://www.webmoney.ru/">WebMoney</a>,
+                    <a rel="nofollow" target="_blank" href="https://www.paypal.com/nl/webapps/mpp/home">PayPal</a>,
+                    <a rel="nofollow" target="_blank" href="http://epayservices.com/">ePayService</a>,
+                    <a rel="nofollow" target="_blank" href="http://ecoin.cc/">eCoin</a>,
+                    <a rel="nofollow" href="https://www.paxum.com/payment/index.php?view=views/index.xsl">Paxum</a>,
+                    <a rel="nofollow" target="_blank" href="https://www.interkassa.com/">InterKassa</a>
+                </noindex>
+                <?= Yii::t('hipanel:site:domain', 'as well as PayPal payments from your Visa and MasterCard') ?>
+            </p>
+        </div>
+    </div>
+</div>
+
+
