f32066539602623fd30667d4f26e2f027008ade6
tofid <andreyklochok@gmail.com>
2017-02-08 19:14:16 +0200

    Added views for Whois

---
+++ src/themes/sailor/whois/_view.php
@@ -0,0 +1,91 @@
+<?php
+
+/** @var string $sShotSrc */
+/** @var \hipanel\modules\domain\models\Domain $model */
+
+use hipanel\modules\domain\widgets\WhoisData;
+use hipanel\widgets\ArraySpoiler;
+use toriphes\lazyload\LazyLoad;
+use yii\widgets\DetailView;
+
+$this->registerCss("
+.shot-img {
+    background-color: #f4f4f4;
+    width: 520px;
+    height: 325px;
+}
+");
+?>
+<?php if ($model->ip) : ?>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="md-mb-10 text-center">
+                <?= LazyLoad::widget([
+                    'src' => $model->screenshot,
+                    'options' => [
+                        'class' => 'img-thumbnail shot-img',
+                        'alt' => $model->domain,
+                    ]
+                ]) ?>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'domain',
+                    [
+                        'attribute' => 'created',
+                        'format' => 'date',
+                        'visible' => !empty($model->created),
+                    ],
+                    [
+                        'attribute' => 'updated',
+                        'format' => 'date',
+                        'visible' => !empty($model->updated),
+                    ],
+                    [
+                        'attribute' => 'expires',
+                        'format' => 'date',
+                        'visible' => !empty($model->expires),
+                    ],
+                    [
+                        'attribute' => 'registrar',
+                        'visible' => !empty($model->expires),
+                    ],
+                    [
+                        'attribute' => 'nss',
+                        'value' => ArraySpoiler::widget([
+                            'data' => $model->nss,
+                            'visibleCount' => count($model->nss),
+                            'formatter' => function ($ip, $ns) {
+                                return $ns . ' - ' . $ip;
+                            },
+                            'delimiter' => '<br>',
+                        ]),
+                        'format' => 'html',
+                    ],
+                    [
+                        'attribute' => 'ip',
+                    ],
+                    [
+                        'attribute' => 'country_name',
+                    ],
+                    [
+                        'attribute' => 'city',
+                        'visible' => !empty($model->city),
+                    ],
+                ]
+            ]) ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12" style="font-family: monospace">
+            <div class="well well-sm"><?= WhoisData::widget(['data' => $model->rawdata]) ?></div>
+        </div>
+    </div>
+<?php else: ?>
+    <div class="alert alert-danger text-center">
+        <?= Yii::t('hipanel/domain', 'You have entered wrong domain name or domain name with unsupported zone.') ?>
+    </div>
+<?php endif ?>
---
+++ src/themes/sailor/whois/index.php
@@ -0,0 +1,71 @@
+<?php
+
+/** @var string $domain */
+/** @var array $availableZones */
+/** @var \hipanel\modules\domain\models\Whois $model */
+/** @var \yii\web\View $this */
+
+use hipanel\modules\domain\assets\WhoisAsset;
+use hipanel\site\widgets\WhoisLookupForm;
+use hipanel\widgets\ArraySpoiler;
+use yii\bootstrap\Html;
+
+$this->title = Yii::t('hipanel/domain', 'WHOIS lookup');
+
+$this->registerCss("
+#whois-lookup h2 {
+    text-align: center;
+    color: #fff;
+    font-weight: 400;
+    font-size: 16px;
+    margin-top: 15px;
+}
+.progress {
+    height: 20px;
+}
+");
+if ($model->domain !== null) {
+    WhoisAsset::register($this);
+    $this->registerJs("$('#whois').whois({domain: '{$model->domain}'});");
+}
+?>
+
+<?php $this->beginBlock('subHeader') ?>
+<div id="whois-lookup" class="domainavailability">
+    <div class="row">
+        <div class="col-sm-12 col-md-9 center-block">
+            <?= Html::tag('h1', $this->title, ['class' => 'text-center']) ?>
+            <div class="domain-form-container">
+                <?= WhoisLookupForm::widget(['model' => $model]) ?>
+            </div>
+            <div class="clearfix"></div>
+            <?= Html::tag('h2', Yii::t('hipanel/domain', 'Available zones') .
+                ': ' .
+                ArraySpoiler::widget(['data' => $availableZones, 'visibleCount' => count($availableZones)])) ?>
+        </div>
+    </div>
+</div>
+<?php $this->endBlock() ?>
+
+<div class="blog">
+
+    <div class="row">
+        <div class="col-sm-12">
+            <article>
+                <div class="post-content">
+                    <div id="whois">
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-striped active" role="progressbar"
+                                 aria-valuenow="100"
+                                 aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 100%">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+
+</div>
+
