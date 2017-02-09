887638b6760091a2ac072bbbcfc166bb2b666447
tofid <andreyklochok@gmail.com>
2017-02-09 21:13:04 +0200

    Fixed marckup

--- src/themes/sailor/site/_domainSearchForm.php
+++ src/themes/sailor/site/_domainSearchForm.php
@@ -2,20 +2,22 @@
 use hipanel\site\widgets\DomainSearchForm;
 
 ?>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="text-center">
-                <h2><?= $this->title ?></h2>
-            </div>
-            <?= DomainSearchForm::widget([
-                'template' => sprintf('<div class="form-group multiple-form-group input-group">
+<section class="callaction">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="text-center">
+                    <h2><?= $this->title ?></h2>
+                </div>
+                <?= DomainSearchForm::widget([
+                    'template' => sprintf('<div class="form-group multiple-form-group input-group">
                             {input}
                             <span class="input-group-btn">
                                 <button type="submit" class="btn btn-theme btn-add">%s</button>
                             </span>
                         </div>{error}', Yii::t('hipanel:domain', 'Domain search')),
-            ]) ?>
+                ]) ?>
+            </div>
         </div>
     </div>
-</div>
+</section>
