abae8ba3591f15b57a12c5ea295dbd4fa1246ff3
tofid <andreyklochok@gmail.com>
2017-02-10 17:44:07 +0200

    Fixed murckup for Faq page

--- src/themes/sailor/widgets/faq/root.php
+++ src/themes/sailor/widgets/faq/root.php
@@ -26,10 +26,6 @@ $('#faq').faq();
 ");
 
 $this->registerCss("
-.tab-content > .tab-pane {
-    padding: 0;
-    border: none;
-}
 .faq-tabs-white {
     padding: 0;
 }
@@ -38,40 +34,44 @@ $this->registerCss("
     background: none;
     overflow: hidden;
 }
-
+.faq-categories ul li a span.badge {
+    margin-left: 5px;
+    font-size: 11px;
+    background: #b2b2b4;
+    padding: 4px 7px 5px;
+}
+.badge {
+    border-radius: 4px;
+}
 ");
 ?>
 
-<div class="row">
-    <div class="col-md-12">
-        <div id="faq" class="faq-tabs">
-            <div class="faq-categories">
-                <ul>
-                    <?php foreach ($items as $tabId => $tab): ?>
-                        <li>
-                            <?= Html::a(sprintf('%s<span class="badge">%d</span>', $tab['label'], count($tab['items'])), '#tab-' . $tabId, ['data-toggle' => 'tab']) ?>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
 <div class="faq-tabs-white">
     <div class="row">
         <div class="col-md-12">
-            <div class="tabbable tabs-top-horizontal">
-                <div class="tab-content">
-                    <?php foreach ($items as $tabId => $tab) : ?>
-                        <div class="tab-pane fade" id="<?= 'tab-' . $tabId ?>">
-                            <div id="<?= $tabId ?>" class="panel-group spacing-40">
-                                <?php foreach ($tab['items'] as $itemId => $item) : ?>
-                                    <?= $this->render('node', ['item' => $item, 'itemId' => $itemId, 'parentId' => $tabId]) ?>
-                                <?php endforeach; ?>
+            <div id="faq" class="faq-tabs">
+                <div class="faq-categories">
+                    <ul class="nav nav-tabs">
+                        <?php foreach ($items as $tabId => $tab): ?>
+                            <li>
+                                <?= Html::a(sprintf('%s<span class="badge">%d</span>', $tab['label'], count($tab['items'])), '#tab-' . $tabId, ['data-toggle' => 'tab']) ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <div class="tab-content">
+                        <?php foreach ($items as $tabId => $tab) : ?>
+                            <div class="tab-pane fade" id="<?= 'tab-' . $tabId ?>">
+                                <div id="<?= $tabId ?>" class="panel-group spacing-40">
+                                    <?php foreach ($tab['items'] as $itemId => $item) : ?>
+                                        <?= $this->render('node', ['item' => $item, 'itemId' => $itemId, 'parentId' => $tabId]) ?>
+                                    <?php endforeach; ?>
+                                </div>
                             </div>
-                        </div>
-                    <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
+            </div>
+            <div class="tabbable tabs-top-horizontal">
             </div>
         </div>
     </div>
