9144b8faab947eae28cf1b3b5c78255d559163ca
tofid <andreyklochok@gmail.com>
2017-02-14 18:46:39 +0200

    Fixed marckup for check domain

--- src/themes/sailor/check/checkDomain.php
+++ src/themes/sailor/check/checkDomain.php
@@ -151,110 +151,91 @@ $this->blocks['dropDownZonesOptions'] = $dropDownZonesOptions;
 ?>
 
 <?php $this->beginBlock('subHeader') ?>
-    <?= $this->render('//site/_domainSearchForm') ?>
+<?= $this->render('//site/_domainSearchForm') ?>
 <?php $this->endBlock() ?>
 
-<div class="blog">
-    <div class="row">
-        <div class="col-sm-8">
-            <article>
-                <div class="post-content">
-                    <div class="domain-list">
-                        <?php foreach ($results as $line) : ?>
-                            <?= $this->render('_checkDomainLine', ['line' => $line, 'requestedDomain' => $requestedDomain]) ?>
-                        <?php endforeach; ?>
-                    </div>
-                </div>
-            </article>
+<div class="row">
+    <div class="col-sm-8">
+        <div class="domain-list">
+            <?php foreach ($results as $line) : ?>
+                <?= $this->render('_checkDomainLine', ['line' => $line, 'requestedDomain' => $requestedDomain]) ?>
+            <?php endforeach; ?>
         </div>
+    </div>
 
-        <div class="col-sm-4 filters">
-            <div class="sidebar">
+    <div class="col-sm-4 filters">
+        <aside class="right-sidebar">
 
-                <div class="widget">
-                    <h3 class="badge"><?= Yii::t('hipanel/domainchecker', 'Status') ?></h3>
-                    <ul class="filter-nav" data-filter-group="status">
-                        <li class="active"><a href="#" data-filter=""><?= Yii::t('hipanel/domainchecker', 'All') ?></a>
-                        </li>
-                        <li><a href="#" data-filter=".available"><?= Yii::t('hipanel/domainchecker', 'Available') ?></a>
-                        </li>
-                        <li><a href="#"
-                               data-filter=".unavailable"><?= Yii::t('hipanel/domainchecker', 'Unavailable') ?></a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="widget">
-                    <h3 class="badge"><?= Yii::t('hipanel/domainchecker', 'Special') ?></h3>
-                    <ul class="filter-nav" data-filter-group="status">
-                        <li class="active"><a href="#" data-filter=""><?= Yii::t('hipanel/domainchecker', 'All') ?></a>
-                        </li>
-                        <li><a href="#"
-                               data-filter=".popular"><?= Yii::t('hipanel/domainchecker', 'Popular Domains') ?></a></li>
-                        <li><a href="#" data-filter=".promotion"><?= Yii::t('hipanel/domainchecker', 'Promotion') ?></a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="widget">
-                    <h3 class="badge"><?= Yii::t('hipanel/domainchecker', 'Categories') ?></h3>
-                    <ul class="filter-nav" data-filter-group="status">
-                        <li class="active">
-                            <a href="#" data-filter=""><?= Yii::t('hipanel/domainchecker', 'All') ?>
-                                <span class="badge"><?= count($results) ?></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" data-filter=".adult"><?= Yii::t('hipanel/domainchecker', 'Adult') ?>
-                                <span class="badge"><?= Domain::getCategoriesCount('adult', $results) ?></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" data-filter=".geo"><?= Yii::t('hipanel/domainchecker', 'GEO') ?>
-                                <span class="badge"><?= Domain::getCategoriesCount('geo', $results) ?></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" data-filter=".general"><?= Yii::t('hipanel/domainchecker', 'General') ?>
-                                <span class="badge"><?= Domain::getCategoriesCount('general', $results) ?></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" data-filter=".nature"><?= Yii::t('hipanel/domainchecker', 'Nature') ?>
-                                <span class="badge"><?= Domain::getCategoriesCount('nature', $results) ?></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" data-filter=".internet"><?= Yii::t('hipanel/domainchecker', 'Internet') ?>
-                                <span class="badge"><?= Domain::getCategoriesCount('internet', $results) ?></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" data-filter=".sport"><?= Yii::t('hipanel/domainchecker', 'Sport') ?>
-                                <span class="badge"><?= Domain::getCategoriesCount('sport', $results) ?></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" data-filter=".society"><?= Yii::t('hipanel/domainchecker', 'Society') ?>
-                                <span class="badge"><?= Domain::getCategoriesCount('society', $results) ?></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" data-filter=".audio_music"><?= Yii::t('hipanel/domainchecker', 'Audio&Music') ?>
-                                <span class="badge"><?= Domain::getCategoriesCount('audio_music', $results) ?></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" data-filter=".home_gifts"><?= Yii::t('hipanel/domainchecker', 'Home&Gifts') ?>
-                                <span class="badge"><?= Domain::getCategoriesCount('home_gifts', $results) ?></span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
+            <div class="widget">
+                <h5 class="widgetheading"><?= Yii::t('hipanel/domainchecker', 'Status') ?></h5>
+                <ul class="cat filter-nav" data-filter-group="status">
+                    <li class="active"><a href="#" data-filter=""><?= Yii::t('hipanel/domainchecker', 'All') ?></a>
+                    </li>
+                    <li><a href="#" data-filter=".available"><?= Yii::t('hipanel/domainchecker', 'Available') ?></a>
+                    </li>
+                    <li><a href="#"
+                           data-filter=".unavailable"><?= Yii::t('hipanel/domainchecker', 'Unavailable') ?></a>
+                    </li>
+                </ul>
             </div>
-        </div>
 
+            <div class="widget">
+                <h5 class="widgetheading"><?= Yii::t('hipanel/domainchecker', 'Special') ?></h5>
+                <ul class="cat filter-nav" data-filter-group="status">
+                    <li class="active"><a href="#" data-filter=""><?= Yii::t('hipanel/domainchecker', 'All') ?></a>
+                    </li>
+                    <li><a href="#"
+                           data-filter=".popular"><?= Yii::t('hipanel/domainchecker', 'Popular Domains') ?></a></li>
+                    <li><a href="#" data-filter=".promotion"><?= Yii::t('hipanel/domainchecker', 'Promotion') ?></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="widget">
+                <h5 class="widgetheading"><?= Yii::t('hipanel/domainchecker', 'Categories') ?></h5>
+                <ul class="cat filter-nav" data-filter-group="status">
+                    <li class="active">
+                        <a href="#" data-filter=""><?= Yii::t('hipanel/domainchecker', 'All') ?></a>
+                        <span><?= count($results) ?></span>
+                    </li>
+                    <li>
+                        <a href="#" data-filter=".adult"><?= Yii::t('hipanel/domainchecker', 'Adult') ?></a>
+                        <span><?= Domain::getCategoriesCount('adult', $results) ?></span>
+                    </li>
+                    <li>
+                        <a href="#" data-filter=".geo"><?= Yii::t('hipanel/domainchecker', 'GEO') ?></a>
+                        <span><?= Domain::getCategoriesCount('geo', $results) ?></span>
+                    </li>
+                    <li>
+                        <a href="#" data-filter=".general"><?= Yii::t('hipanel/domainchecker', 'General') ?></a>
+                        <span><?= Domain::getCategoriesCount('general', $results) ?></span>
+                    </li>
+                    <li>
+                        <a href="#" data-filter=".nature"><?= Yii::t('hipanel/domainchecker', 'Nature') ?></a>
+                        <span><?= Domain::getCategoriesCount('nature', $results) ?></span>
+                    </li>
+                    <li>
+                        <a href="#" data-filter=".internet"><?= Yii::t('hipanel/domainchecker', 'Internet') ?></a>
+                        <span><?= Domain::getCategoriesCount('internet', $results) ?></span>
+                    </li>
+                    <li>
+                        <a href="#" data-filter=".sport"><?= Yii::t('hipanel/domainchecker', 'Sport') ?></a>
+                        <span><?= Domain::getCategoriesCount('sport', $results) ?></span>
+                    </li>
+                    <li>
+                        <a href="#" data-filter=".society"><?= Yii::t('hipanel/domainchecker', 'Society') ?></a>
+                        <span><?= Domain::getCategoriesCount('society', $results) ?></span>
+                    </li>
+                    <li>
+                        <a href="#" data-filter=".audio_music"><?= Yii::t('hipanel/domainchecker', 'Audio&Music') ?></a>
+                        <span><?= Domain::getCategoriesCount('audio_music', $results) ?></span>
+                    </li>
+                    <li>
+                        <a href="#" data-filter=".home_gifts"><?= Yii::t('hipanel/domainchecker', 'Home&Gifts') ?></a>
+                        <span><?= Domain::getCategoriesCount('home_gifts', $results) ?></span>
+                    </li>
+                </ul>
+            </div>
+        </aside>
     </div>
 </div>
-<!-- End of Blog -->
