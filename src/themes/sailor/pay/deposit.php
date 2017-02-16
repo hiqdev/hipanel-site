1a9c2594936041e61d36f8d3f31a25f3bfe4bb19
tofid <andreyklochok@gmail.com>
2017-02-16 18:50:41 +0200

    Fixed breadcrump on the select pay page

--- src/themes/sailor/pay/deposit.php
+++ src/themes/sailor/pay/deposit.php
@@ -4,7 +4,7 @@ use hiqdev\yii2\merchant\widgets\PayButton;
 use yii\helpers\Html;
 
 $this->title = Yii::t('merchant', 'Select payment method');
-$this->params['breadcrumbs'][] = ['label' => Yii::t('merchant', 'Recharge account'), 'url' => ['deposit']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('merchant', 'Recharge account'), 'url' => ['/merchant/pay/deposit']];
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
