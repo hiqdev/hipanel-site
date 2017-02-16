f640c44084456d586c9226303514bfa3f8a67709
tofid <andreyklochok@gmail.com>
2017-02-16 18:39:35 +0200

    Fixed marckup for deposit view

--- src/themes/sailor/pay/deposit.php
+++ src/themes/sailor/pay/deposit.php
@@ -9,5 +9,20 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-123
+<div class="row">
+    <div class="col-md-offset-3 col-md-6">
+        <div class="panel <?= empty($requests) ? 'panel-danger' : '' ?>">
+            <div class="panel-body">
+                <?php if (empty($requests)) : ?>
+                    <?= Html::tag('div', Yii::t('merchant', 'There are no payments methods available'), ['class' => 'alert alert-danger']) ?>
+                <?php else : ?>
+                    <?php foreach ($requests as $request) : ?>
+                        <?= PayButton::widget(compact('request')) ?>
+                        <br/>
+                    <?php endforeach ?>
+                <?php endif ?>
+            </div>
+        </div>
+    </div>
+</div>
 
