31d4d7241728abbf08539a63c70a27da82e033db
tofid <andreyklochok@gmail.com>
2017-02-16 17:32:22 +0200

    Added views for Sailor theme

---
+++ src/themes/sailor/pay/deposit.php
@@ -0,0 +1,13 @@
+<?php
+
+use hiqdev\yii2\merchant\widgets\PayButton;
+use yii\helpers\Html;
+
+$this->title = Yii::t('merchant', 'Select payment method');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('merchant', 'Recharge account'), 'url' => ['deposit']];
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+
+123
+
