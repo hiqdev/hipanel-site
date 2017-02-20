f9d09b34a19f6f125012ffc4574e138b182f294e
tofid <andreyklochok@gmail.com>
2017-02-20 19:49:43 +0200

    Fixed marckup and translation dictionaries

--- src/themes/sailor/whois/index.php
+++ src/themes/sailor/whois/index.php
@@ -10,7 +10,8 @@ use hipanel\site\widgets\WhoisLookupForm;
 use hipanel\widgets\ArraySpoiler;
 use yii\bootstrap\Html;
 
-$this->title = Yii::t('hipanel/domain', 'WHOIS lookup');
+$this->title = Yii::t('hipanel:domain', 'WHOIS lookup');
+$this->params['breadcrumbs'][] = $this->title;
 
 $this->registerCss("
 #whois-lookup h2 {
@@ -31,41 +32,39 @@ if ($model->domain !== null) {
 ?>
 
 <?php $this->beginBlock('subHeader') ?>
-<div id="whois-lookup" class="domainavailability">
-    <div class="row">
-        <div class="col-sm-12 col-md-9 center-block">
-            <?= Html::tag('h1', $this->title, ['class' => 'text-center']) ?>
-            <div class="domain-form-container">
-                <?= WhoisLookupForm::widget(['model' => $model]) ?>
+    <section class="callaction">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <?= WhoisLookupForm::widget(['model' => $model]) ?>
+                    <div class="text-center">
+                        <?= Html::tag('b', Yii::t('hipanel:domain', 'Available zones') .
+                            ': ' .
+                            ArraySpoiler::widget(['data' => $availableZones, 'visibleCount' => count($availableZones)])) ?>
+                    </div>
+                </div>
             </div>
-            <div class="clearfix"></div>
-            <?= Html::tag('h2', Yii::t('hipanel/domain', 'Available zones') .
-                ': ' .
-                ArraySpoiler::widget(['data' => $availableZones, 'visibleCount' => count($availableZones)])) ?>
         </div>
-    </div>
-</div>
+    </section>
 <?php $this->endBlock() ?>
 
-<div class="blog">
 
-    <div class="row">
-        <div class="col-sm-12">
-            <article>
-                <div class="post-content">
-                    <div id="whois">
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-striped active" role="progressbar"
-                                 aria-valuenow="100"
-                                 aria-valuemin="0"
-                                 aria-valuemax="100" style="width: 100%">
-                            </div>
+<div class="row">
+    <div class="col-sm-12">
+        <article>
+            <div class="post-content">
+                <div id="whois">
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar"
+                             aria-valuenow="100"
+                             aria-valuemin="0"
+                             aria-valuemax="100" style="width: 100%">
                         </div>
                     </div>
                 </div>
-            </article>
-        </div>
+            </div>
+        </article>
     </div>
-
 </div>
+
 
