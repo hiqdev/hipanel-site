53e3a604ddd4196cc557f48671ba941f26090c0f
tofid <andreyklochok@gmail.com>
2017-02-20 19:39:24 +0200

    Added view for WhoisLookupForm widget in Sailor theme

---
+++ src/themes/sailor/check/_checkDomainItem.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @var array
+ * @var $state string
+ */
+use hipanel\modules\domain\models\Domain;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/** @var array $line */
+$addToCartPath = '/site/add-to-cart-registration';
+$topcartUrl = '/cart/cart/topcart';
+?>
+
+<div class="domain-iso-line
+    <?= Domain::setIsotopeFilterValue($line['zone']) ?>
+    <?= $line['isAvailable'] ? 'available' : 'unavailable' ?>
+    <?= $line['fqdn'] === $requestedDomain ? 'popular' : $requestedDomain ?>
+">
+    <div class="domain-line <?= $line['isAvailable'] === true ? 'checked' : '' ?>" data-domain="<?= $line['fqdn'] ?>">
+        <div class="col-md-6 col-sm-12 col-xs-12">
+            <?php if (isset($line['isAvailable'])) : ?>
+                <span class="domain-img"><i class="fa fa-globe"></i></span>
+            <?php else : ?>
+                <span class="domain-img"><i class="fa fa-circle-o-notch fa-spin"></i></span>
+            <?php endif; ?>
+
+            <?php if ($line['isAvailable'] === true) : ?>
+                <span class="domain-name"><?= $line['domain'] ?></span><span
+                        class="domain-zone">.<?= $line['zone'] ?></span>
+            <?php else : ?>
+                <span class="domain-name muted"><?= $line['domain'] ?></span><span
+                        class="domain-zone muted">.<?= $line['zone'] ?></span>
+            <?php endif; ?>
+        </div>
+        <div class="col-md-3 col-sm-12 col-xs-12 text-center">
+            <span class="domain-price">
+                <?php if ($line['isAvailable']) : ?>
+                    <b><?= Yii::$app->formatter->format($line['resource']->price, ['currency', $line['resource']->currency]) ?></b>
+                    <span class="domain-price-year">/ <?= Yii::t('hipanel:domain', 'year') ?></span>
+
+                <?php elseif ($line['isAvailable'] === false) : ?>
+                    <span class="domain-taken">
+                        <?= Yii::t('hipanel:domain', 'Domain is not available') ?>
+                    </span>
+                <?php endif; ?>
+            </span>
+        </div>
+        <div class="col-md-3 col-sm-12 col-xs-12">
+            <?php if ($line['isAvailable'] === true) : ?>
+                <?= Html::a('<i class="fa fa-cart-plus fa-lg"></i>&nbsp; ' . Yii::t('hipanel:domain', 'Add to cart'), ['add-to-cart-registration', 'name' => $line['fqdn']], [
+                    'data-pjax' => 0,
+                    'class' => 'btn btn-theme add-to-cart-button btn-success',
+                    'data-loading-text' => '<i class="fa fa-circle-o-notch fa-spin fa-lg"></i>&nbsp;&nbsp;' . Yii::t('hipanel:domain', 'Adding'),
+                    'data-complete-text' => '<i class="fa fa-check fa-lg"></i>&nbsp;&nbsp;' . Yii::t('hipanel:domain', 'In cart'),
+                    'data-domain-url' => Url::to([$addToCartPath, 'name' => $line['fqdn']]),
+                    'data-topcart' => Url::toRoute([$topcartUrl]),
+                ]) ?>
+            <?php elseif ($line['isAvailable'] === false) : ?>
+                <?= Html::a('<i class="fa fa-search"></i>&nbsp; ' . Yii::t('hipanel:domain', 'WHOIS'), ['/domain/whois/index', 'domain' => $line['fqdn']], ['target' => '_blank', 'class' => 'btn btn-default btn-flat']) ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
---
+++ src/themes/sailor/widgets/WhoisLookupForm.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Url;
+
+/** @var \hipanel\modules\domain\models\Whois $model */
+
+?>
+
+
+<?php $form = ActiveForm::begin([
+    'id' => 'whois-lookup',
+    'action' => Url::toRoute('whois/index'),
+    'method' => 'get',
+    'options' => [
+        'data-pjax' => false,
+        'class' => 'material',
+    ],
+    'fieldConfig' => [
+        'template' => sprintf('<div class="form-group multiple-form-group input-group">
+                            {input}
+                            <span class="input-group-btn">
+                                <button type="submit" class="btn btn-theme btn-add">%s</button>
+                            </span>
+                        </div>{error}', Yii::t('hipanel:domain', 'Lookup')),
+    ],
+]) ?>
+<div class="row">
+    <div class="col-sm-12">
+        <?= $form->field($model, 'domain')->textInput([
+            'placeholder' => Yii::t('hipanel:domain', 'Domain name'),
+            'class' => 'form-control',
+            'name' => 'domain',
+            'id' => 'domain',
+            'autocomplete' => 'off',
+        ]) ?>
+    </div>
+</div>
+<?php ActiveForm::end() ?>
