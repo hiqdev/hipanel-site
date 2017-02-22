e6311400cb1f2f7ce737975cb4e0b5792e3b4120
tofid <andreyklochok@gmail.com>
2017-02-22 17:17:51 +0200

    Added common for all views _domainSearchForm

---
+++ src/themes/dataserv/site/_domainSearchForm.php
@@ -0,0 +1,18 @@
+<?php
+use hipanel\site\widgets\DomainSearchForm;
+use yii\helpers\Html;
+
+?>
+<div class="domainavailability">
+    <div class="row">
+        <div class="col-sm-12 col-md-9 center-block">
+            <?= Html::tag('h1', $this->title, ['class' => 'text-center']) ?>
+            <div class="domain-form-container">
+                <?= DomainSearchForm::widget([
+                    'model' => $model,
+                    'template' => "<div class=\"row\"><input type=\"hidden\" name=\"direct\" value=\"true\"/><div class=\"col-sm-11\">{input}\n{error}</div><div class=\"col-sm-1\"><button class=\"mtr-btn button-fab\" type=\"submit\"><i class=\"fa fa-search\"></i></button></div></div>",
+                ]) ?>
+            </div>
+        </div>
+    </div>
+</div>
