26d4f8f60c08e1960aa50a6a43faec60013e2849
tofid <andreyklochok@gmail.com>
2017-02-22 13:06:05 +0200

    Fixed mackup

--- src/themes/dataserv/site/index.php
+++ src/themes/dataserv/site/index.php
@@ -7,18 +7,19 @@ use yii\helpers\Html;
 $this->title = Yii::t('hipanel:site:domain', 'Domain names search and registration');
 ?>
 <?php $this->beginBlock('subHeader') ?>
-    <div class="domainavailability">
-        <div class="row">
-            <div class="col-sm-12 col-md-9 center-block">
-                <?= Html::tag('h1', $this->title, ['class' => 'text-center']) ?>
-                <div class="domain-form-container">
-                    <?= DomainSearchForm::widget([
-                        'template' => "<div class=\"row\"><input type=\"hidden\" name=\"direct\" value=\"true\"/><div class=\"col-sm-11\">{input}\n{error}</div><div class=\"col-sm-1\"><button class=\"mtr-btn button-fab\" type=\"submit\"><i class=\"fa fa-search\"></i></button></div></div>",
-                    ]) ?>
-                </div>
+<div class="domainavailability">
+    <div class="row">
+        <div class="col-sm-12 col-md-9 center-block">
+            <?= Html::tag('h1', $this->title, ['class' => 'text-center']) ?>
+            <div class="domain-form-container">
+                <?= DomainSearchForm::widget([
+                    'model' => $model,
+                    'template' => "<div class=\"row\"><input type=\"hidden\" name=\"direct\" value=\"true\"/><div class=\"col-sm-11\">{input}\n{error}</div><div class=\"col-sm-1\"><button class=\"mtr-btn button-fab\" type=\"submit\"><i class=\"fa fa-search\"></i></button></div></div>",
+                ]) ?>
             </div>
         </div>
     </div>
+</div>
 <?php $this->endBlock() ?>
 
 <div class="domaintlds">
