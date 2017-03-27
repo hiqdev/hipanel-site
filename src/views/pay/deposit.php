e411e5e98d2e7dc736da8fff72e04e0f0daecda8
tofid <andreyklochok@gmail.com>
2017-03-27 14:04:03 +0300

    Modified deposit view markup and added inline css

--- src/views/pay/deposit.php
+++ src/views/pay/deposit.php
@@ -5,20 +5,27 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('merchant', 'Select payment method');
 $this->params['breadcrumbs'][] = $this->title;
-
+$this->registerCss("
+.products-list .product-description {
+    font-size: 12px;
+}
+");
 ?>
 
 <div class="row">
-    <div class="col-md-offset-3 col-md-6">
+    <div class="col-md-offset-2 col-md-8">
         <div class="panel <?= empty($requests) ? 'panel-danger' : '' ?>">
             <div class="panel-body">
                 <?php if (empty($requests)) : ?>
                     <?= Html::tag('div', Yii::t('merchant', 'There are no payments methods available'), ['class' => 'alert alert-danger']) ?>
                 <?php else : ?>
-                    <?php foreach ($requests as $request) : ?>
-                        <?= PayButton::widget(compact('request')) ?>
-                        <br/>
-                    <?php endforeach ?>
+	                <ul class="products-list product-list-in-box">
+                        <?php foreach ($requests as $request) : ?>
+                            <li class="item">
+                                <?= PayButton::widget(compact('request')) ?>
+                            </li>
+                        <?php endforeach ?>
+                    </ul>
                 <?php endif ?>
             </div>
         </div>
