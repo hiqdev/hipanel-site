9cecf6ba60a597a588665363ae42e36ecc0ff2d9
tofid <andreyklochok@gmail.com>
2017-03-28 12:27:07 +0300

    Fixed old translations

--- src/pages/rules/_cancelation_policy.php
+++ src/pages/rules/_cancelation_policy.php
@@ -1,30 +1,30 @@
 <p>
-    <?= Yii::t('hipanel:pages', 'All AHnames.com services are operating on a prepay basis. AHnames.com customers may decide to cancel their accounts at any time.') ?>
+    All AHnames.com services are operating on a prepay basis. AHnames.com customers may decide to cancel their accounts at any time.
 </p>
 
 <p>
-    <?= Yii::t('hipanel:pages', 'Refund is offered only for the VDS service if the cancellation request is made within 5 (five) days of purchasing/renewal/date of billing of the service, according to our Money Back Guarantee Policy.') ?>
-    <?= Yii::t('hipanel:pages', 'To cancel a service, the Customer needs to use a "cancel button" in his/her web hosting Control Panel.') ?>
-    <?= Yii::t('hipanel:pages', 'Any incentives (free bonuses, etc.) offered to the Customer upon sign-up will be also cancelled.') ?>
+    Refund is offered only for the VDS service if the cancellation request is made within 5 (five) days of purchasing/renewal/date of billing of the service, according to our Money Back Guarantee Policy.
+    To cancel a service, the Customer needs to use a "cancel button" in his/her web hosting Control Panel.
+    Any incentives (free bonuses, etc.) offered to the Customer upon sign-up will be also cancelled.
 </p>
 
 <p>
-    <?= Yii::t('hipanel:pages', 'Domain name registrations/transfers, service upgrades, additional VDS services (ip, traffic) are treated as non-refundable services and cannot be cancelled or refunded after being activated upon customer request.') ?>
+    Domain name registrations/transfers, service upgrades, additional VDS services (ip, traffic) are treated as non-refundable services and cannot be cancelled or refunded after being activated upon customer request.
 </p>
 
-<h5><?= Yii::t('hipanel:pages', 'Money-back guarantee policy') ?></h5>
+<h5>Money-back guarantee policy</h5>
 
 <p>
-    <?= Yii::t('hipanel:pages', 'AHnames.com offers a 5-day money back guarantee.') ?>
+    AHnames.com offers a 5-day money back guarantee.
 </p>
 
 <p>
-    <?= Yii::t('hipanel:pages', 'If, for any reason, you are dissatisfied with our VDS services, we will refund your hosting fees at any time to your account during the first 5(five) days after the purchasing/renewal/date of billing of the service. You can submit a refund request in a ticket from your web hosting Control Panel.') ?>
+    If, for any reason, you are dissatisfied with our VDS services, we will refund your hosting fees at any time to your account during the first 5(five) days after the purchasing/renewal/date of billing of the service. You can submit a refund request in a ticket from your web hosting Control Panel.
 </p>
 
-<h5><?= Yii::t('hipanel:pages', 'Non-refundable services') ?></h5>
+<h5>Non-refundable services</h5>
 
 <ul>
-    <li><?= Yii::t('hipanel:pages', 'All upgrades and additional services (ip, traffic).') ?></li>
-    <li><?= Yii::t('hipanel:pages', 'Domain registrations, renewals and transfers, purchased from AHnames Control Panel.') ?></li>
+    <li>All upgrades and additional services (ip, traffic).</li>
+    <li>Domain registrations, renewals and transfers, purchased from AHnames Control Panel.</li>
 </ul>
--- src/pages/rules/_privacy_policy.php
+++ src/pages/rules/_privacy_policy.php
@@ -2,127 +2,126 @@
 use \yii\helpers\Html;
 ?>
 <p>
-    <?= Yii::t('hipanel:pages', 'We guarantee confidentiality when storing and processing the obtained personal information.') ?>
-    <?= Yii::t('hipanel:pages', 'The following rules describe how AHnames.com collects, uses and discloses the personal and non-personal information you provide while using the "Site".') ?>
+    We guarantee confidentiality when storing and processing the obtained personal information.
+    The following rules describe how AHnames.com collects, uses and discloses the personal and non-personal information you provide while using the "Site".
 </p>
 
 <p>
-    <?= Yii::t('hipanel:pages', 'By using AHnames.com, you agree to accept the practices described in this Privacy Policy.') ?>
-    <?= Yii::t('hipanel:pages', 'This Privacy Policy is subject to AHnames’ Terms &amp; Conditions. If you have any questions concerning the Privacy Policy, please {0}.', [
-        Html::a(Yii::t('hipanel:pages', 'Contact Us'), ['/site/contact'])
-    ]) ?>
+    By using AHnames.com, you agree to accept the practices described in this Privacy Policy.
+    This Privacy Policy is subject to AHnames’ Terms &amp; Conditions. If you have any questions concerning the Privacy Policy, please
+        <?= Html::a(Yii::t('hipanel:pages', 'Contact Us'), ['/site/feedback']) ?>
 </p>
 
-<h5><?= Yii::t('hipanel:pages', 'Automatically Collected Information') ?></h5>
+<h5>Automatically Collected Information</h5>
 
 <p>
-    <?= Yii::t('hipanel:pages', 'We receive and store any information you enter on our Site, or that you give us in any other way, such as via
+    We receive and store any information you enter on our Site, or that you give us in any other way, such as via
     telephone, e-mail, online or otherwise. This information may also be collected when you register a domain name or
-    when you Contact Us for customer support.') ?>
+    when you Contact Us for customer support.
 </p>
 
-<h5><?= Yii::t('hipanel:pages', 'Information Disclosure') ?></h5>
+<h5>Information Disclosure</h5>
 
 <p>
-    <?= Yii::t('hipanel:pages', 'AHnames.com may disclose your personal information as reasonably required by the rules, policies, procedures, and
+    AHnames.com may disclose your personal information as reasonably required by the rules, policies, procedures, and
     guidelines (including domain name dispute policies and rules) adopted and amended by domain name registration
     authorities (such as the Internet Corporation for Assigned Names and Numbers and the Canadian Internet Registration
     Authority), including making certain of your personal information publicly available on searchable databases
-    (including WHOIS databases) relating to domain name registrations.') ?>
+    (including WHOIS databases) relating to domain name registrations.
 </p>
 
-<p><?= Yii::t('hipanel:pages', 'The following information may be disclosed') ?>:</p>
+<p>The following information may be disclosed:</p>
 
 <ul>
-    <li><?= Yii::t('hipanel:pages', 'Domain names registered on your name;') ?></li>
+    <li>Domain names registered on your name;</li>
     <li>
-        <?= Yii::t('hipanel:pages', 'Your name, postal address, email address, telephone numbers and fax numbers of the technical and administrative contacts for the registered domain names;') ?>
+        Your name, postal address, email address, telephone numbers and fax numbers of the technical and administrative contacts for the registered domain names;
     </li>
     <li>
-        <?= Yii::t('hipanel:pages', 'the full hostnames and internet protocol (IP) addresses of at least two nameserver
+        the full hostnames and internet protocol (IP) addresses of at least two nameserver
         hosts (one primary and at least one secondary) for each registered domain names; the corresponding names of
-        those nameservers;') ?>
+        those nameservers;
     </li>
-    <li><?= Yii::t('hipanel:pages', 'Internet protocol (IP) addresses and name server (NS) hosts for each registered domain name;') ?></li>
-    <li><?= Yii::t('hipanel:pages', 'The corresponding names of those name servers;') ?></li>
-    <li><?= Yii::t('hipanel:pages', 'The date of domain registration;') ?></li>
-    <li><?= Yii::t('hipanel:pages', 'The date on which the registration expires.') ?></li>
+    <li>Internet protocol (IP) addresses and name server (NS) hosts for each registered domain name;</li>
+    <li>The corresponding names of those name servers;</li>
+    <li>The date of domain registration;</li>
+    <li>The date on which the registration expires.</li>
 </ul>
 
 <p>
-    <?= Yii::t('hipanel:pages', 'AHnames.com has no control over, and no responsibility or liability for the use or disclosure of your personal
+    AHnames.com has no control over, and no responsibility or liability for the use or disclosure of your personal
     information disclosed to comply with the rules, policies, procedures, and guidelines of domain name registration
-    authorities, and that use and disclosure is not subject to this Policy.') ?>
+    authorities, and that use and disclosure is not subject to this Policy.
 </p>
 
-<h5><?= Yii::t('hipanel:pages', 'Demographic Information') ?></h5>
+<h5>Demographic Information</h5>
 <p>
-    <?= Yii::t('hipanel:pages', 'We may collect and store anonymous demographic information (such as your postal code/zip code, gender and age). Such
+    We may collect and store anonymous demographic information (such as your postal code/zip code, gender and age). Such
     demographic information may be shared with partners, market researchers and other third parties on a mass,
-    non-personally identifiable basis.') ?>
+    non-personally identifiable basis.
 </p>
 <p>
-    <?= Yii::t('hipanel:pages', 'No personally identifiable information will be linked to such aggregated demographic information that is shared with third parties.') ?>
+    No personally identifiable information will be linked to such aggregated demographic information that is shared with third parties.
 </p>
 
 
-<h5><?= Yii::t('hipanel:pages', 'Other Information') ?></h5>
+<h5>Other Information</h5>
 <p>
-    <?= Yii::t('hipanel:pages', 'We collect and store certain types of information whenever you communicate with us, or the Site. For example, our
-    servers track the type of Web browser you are using, and the page from which you link to AHnames.com.') ?>
+    We collect and store certain types of information whenever you communicate with us, or the Site. For example, our
+    servers track the type of Web browser you are using, and the page from which you link to AHnames.com.
 </p>
 <p>
-    <?= Yii::t('hipanel:pages', 'Additionally, we log IP addresses (a unique number assigned to every computer on the Internet) in order to improve
+    Additionally, we log IP addresses (a unique number assigned to every computer on the Internet) in order to improve
     security, analyze trends and administer the Site.
     We may track your movement within the Site, the third-party website pages from which you were referred and access
     time. We use this information to gauge the effectiveness of our Site, track information for statistical purposes and
-    to improve the products and services we provide.') ?>
+    to improve the products and services we provide.
 </p>
 <p>
-    <?= Yii::t('hipanel:pages', 'We may use such information for marketing purposes, and to send information to you that we believe might be of interest.') ?>
+    We may use such information for marketing purposes, and to send information to you that we believe might be of interest.
 </p>
 <p>
-    <?= Yii::t('hipanel:pages', 'In addition, such information may be shared with partners, market researchers and other third parties on an
+    In addition, such information may be shared with partners, market researchers and other third parties on an
     aggregate, non-personally identifiable basis. No personally identifiable information will be linked to information
-    shared with third parties.') ?>
+    shared with third parties.
 </p>
 <p>
-    <?= Yii::t('hipanel:pages', 'We may supplement the information you provide us by acquiring publicly or commercially available information
+    We may supplement the information you provide us by acquiring publicly or commercially available information
     available in the public domain or from third parties to verify your address or to update your information and to
-    create a profile of your preferences.') ?>
+    create a profile of your preferences.
 </p>
 <p>
-    <?= Yii::t('hipanel:pages', 'We may obtain non-sensitive contact information from third parties, including via bulk access licenses from other
+    We may obtain non-sensitive contact information from third parties, including via bulk access licenses from other
     registrars. We may also ask you to participate in surveys or other communications conducted by us or third parties
-    designed to help us improve the Site, or the products or services we provide.') ?>
+    designed to help us improve the Site, or the products or services we provide.
 </p>
 
 
-<h5><?= Yii::t('hipanel:pages', 'Cookies') ?></h5>
+<h5>Cookies</h5>
 <p>
-    <?= Yii::t('hipanel:pages', 'The AHnames.com site uses Cookie technology for user’s convenience and enhanced operation of other functions.
+    The AHnames.com site uses Cookie technology for user’s convenience and enhanced operation of other functions.
     Cookies are text files that are commonly deposited by web sites on a user\'s hard drive when the user visits a web
     site. The file identifies a user\'s computer and allows record the user\'s preferences and other data about the user\'s
-    site visit.') ?>
+    site visit.
 </p>
 <p>
-    <?= Yii::t('hipanel:pages', 'We do not extract information about individual users from Cookies. Also, when Cookies are disabled, certain services may not function correctly.') ?>
+    We do not extract information about individual users from Cookies. Also, when Cookies are disabled, certain services may not function correctly.
 </p>
 <p>
-    <?= Yii::t('hipanel:pages', 'We use third-party advertising companies to serve ads. Some ads may also use Cookies when you click on the
+    We use third-party advertising companies to serve ads. Some ads may also use Cookies when you click on the
     advertised link before getting on our Site, as well as the information about your visits to this and other Web sites
     in order to provide advertisements about goods and services of interest to you. However, no personally identifiable
     information (such as your name, address, email address or telephone number) is offered to advertising companies and
-    third-parties.') ?>
+    third-parties.
 </p>
 
-<h5><?= Yii::t('hipanel:pages', 'How Personal Information Is Used') ?></h5>
+<h5>How Personal Information Is Used</h5>
 <p>
-    <?= Yii::t('hipanel:pages', 'We receive and store any personal information you enter on the Site or give to us voluntarily in any other way. We
+    We receive and store any personal information you enter on the Site or give to us voluntarily in any other way. We
     may use it to contact you. For example, via email, telephone number, SMS, social networks or any other means of
     communication that your wireless or other telecommunications device may be capable of receiving (e.g. video, etc.).
-    Specified email and postal addresses may be used to send you information or offers.') ?>
+    Specified email and postal addresses may be used to send you information or offers.
 </p>
 <p>
-    <?= Yii::t('hipanel:pages', 'If you provide your name for a promotional offer, to enter a sweepstakes or contest, and win a prize, we will post a winner\'s list online, and will file your name with state agencies if required by law.') ?>
+    If you provide your name for a promotional offer, to enter a sweepstakes or contest, and win a prize, we will post a winner\'s list online, and will file your name with state agencies if required by law.
 </p>
