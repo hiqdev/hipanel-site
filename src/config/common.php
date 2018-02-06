a68e1097d1140cbed5b885246e3579dce0331bb5
Andrii Vasyliev <sol@hiqdev.com>
2018-02-06 17:19:48 +0000

    added `common` config

---
+++ src/config/common.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Selling site for HiPanel
+ *
+ * @link      http://hipanel.com/
+ * @package   hipanel-site
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'id' => 'hipanel-site',
+];
